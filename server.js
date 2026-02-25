030c625cce2cc4741e395fac36b6c333a898d85b
princerashed <rashedthe47@gmail.com>
2026-02-25 09:44:22 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
-const express = require('express');
-const fetch = require('node-fetch');
-const cors = require('cors');
-
+const express = require("express");
 const app = express();
-app.use(cors());
-
-app.get('/wingo', async (req, res) => {
-    try {
-        const response = await fetch('https://draw.ar-lottery01.com/WinGo/WinGo_1M.json');
-        const data = await response.json();
-        res.json(data);
-    } catch (error) {
-        res.status(500).json({ error: "API Failed" });
-    }
-});
 
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
 app.listen(PORT, () => {
-    console.log("Server running...");
+  console.log(`Server running on port ${PORT}`);
 });
